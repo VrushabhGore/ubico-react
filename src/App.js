54bef8d0d21a7dc9abc8f75c31e42ad3bb8963cf
Vrushabh Gore <vrushabhgore4@gmail.com>
2020-02-24 14:35:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [value, setvalue] = useState({
+    text:""
+  });
+  const [response,setresponse] = useState([])
+
+  const handleChange = (e) => {
+    setvalue({text:e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:5555',{
+        method:"post",
+        body: JSON.stringify(value),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+    }).then(res => {
+      res.json().then(result =>{
+        setresponse([...response,result])
+      });
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>Ubico Test</h1>
+      <input type ="text" onChange={handleChange} value={value.text}/>
+      <button onClick={handleSubmit}>Submit</button>
+      <table>
+        <th>
+          Text
+        </th>
+        <th>
+          Value
+        </th>
+        {response.map(data => (
+          <React.Fragment>
+          <tr></tr>
+          <td>{data.msg}</td>
+          <td>{data.count}</td>
+          </React.Fragment>
+        ))}
+      </table>
+    </React.Fragment>
   );
 }
 
